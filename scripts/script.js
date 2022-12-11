032e963d018717672c2a7c3d2220ee5c05e05920
PabMatMar <pabmatmar@gmail.com>
2022-12-11 01:48:44 +0100

    Creando elementos div (a razon de flexbox) cada x interaciones del loop

--- scripts/script.js
+++ scripts/script.js
@@ -48,7 +48,325 @@ const books = [
     "pages": 176,
     "title": "The Book Of Job",
     "year": -600
-  }]
+  },
+  {
+    "author": "Chinua Achebe",
+    "country": "Nigeria",
+    "imageLink": "images/things-fall-apart.jpg",
+    "language": "English",
+    "link": "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
+    "pages": 209,
+    "title": "Things Fall Apart",
+    "year": 1958
+  },
+  {
+    "author": "Hans Christian Andersen",
+    "country": "Denmark",
+    "imageLink": "images/fairy-tales.jpg",
+    "language": "Danish",
+    "link": "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n",
+    "pages": 784,
+    "title": "Fairy tales",
+    "year": 1836
+  },
+  {
+    "author": "Dante Alighieri",
+    "country": "Italy",
+    "imageLink": "images/the-divine-comedy.jpg",
+    "language": "Italian",
+    "link": "https://en.wikipedia.org/wiki/Divine_Comedy\n",
+    "pages": 928,
+    "title": "The Divine Comedy",
+    "year": 1315
+  },
+  {
+    "author": "Unknown",
+    "country": "Sumer and Akkadian Empire",
+    "imageLink": "images/the-epic-of-gilgamesh.jpg",
+    "language": "Akkadian",
+    "link": "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh\n",
+    "pages": 160,
+    "title": "The Epic Of Gilgamesh",
+    "year": -1700
+  },
+  {
+    "author": "Unknown",
+    "country": "Achaemenid Empire",
+    "imageLink": "images/the-book-of-job.jpg",
+    "language": "Hebrew",
+    "link": "https://en.wikipedia.org/wiki/Book_of_Job\n",
+    "pages": 176,
+    "title": "The Book Of Job",
+    "year": -600
+  },
+  {
+    "author": "Chinua Achebe",
+    "country": "Nigeria",
+    "imageLink": "images/things-fall-apart.jpg",
+    "language": "English",
+    "link": "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
+    "pages": 209,
+    "title": "Things Fall Apart",
+    "year": 1958
+  },
+  {
+    "author": "Hans Christian Andersen",
+    "country": "Denmark",
+    "imageLink": "images/fairy-tales.jpg",
+    "language": "Danish",
+    "link": "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n",
+    "pages": 784,
+    "title": "Fairy tales",
+    "year": 1836
+  },
+  {
+    "author": "Dante Alighieri",
+    "country": "Italy",
+    "imageLink": "images/the-divine-comedy.jpg",
+    "language": "Italian",
+    "link": "https://en.wikipedia.org/wiki/Divine_Comedy\n",
+    "pages": 928,
+    "title": "The Divine Comedy",
+    "year": 1315
+  },
+  {
+    "author": "Unknown",
+    "country": "Sumer and Akkadian Empire",
+    "imageLink": "images/the-epic-of-gilgamesh.jpg",
+    "language": "Akkadian",
+    "link": "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh\n",
+    "pages": 160,
+    "title": "The Epic Of Gilgamesh",
+    "year": -1700
+  },
+  {
+    "author": "Unknown",
+    "country": "Achaemenid Empire",
+    "imageLink": "images/the-book-of-job.jpg",
+    "language": "Hebrew",
+    "link": "https://en.wikipedia.org/wiki/Book_of_Job\n",
+    "pages": 176,
+    "title": "The Book Of Job",
+    "year": -600
+  },
+  {
+    "author": "Chinua Achebe",
+    "country": "Nigeria",
+    "imageLink": "images/things-fall-apart.jpg",
+    "language": "English",
+    "link": "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
+    "pages": 209,
+    "title": "Things Fall Apart",
+    "year": 1958
+  },
+  {
+    "author": "Hans Christian Andersen",
+    "country": "Denmark",
+    "imageLink": "images/fairy-tales.jpg",
+    "language": "Danish",
+    "link": "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n",
+    "pages": 784,
+    "title": "Fairy tales",
+    "year": 1836
+  },
+  {
+    "author": "Dante Alighieri",
+    "country": "Italy",
+    "imageLink": "images/the-divine-comedy.jpg",
+    "language": "Italian",
+    "link": "https://en.wikipedia.org/wiki/Divine_Comedy\n",
+    "pages": 928,
+    "title": "The Divine Comedy",
+    "year": 1315
+  },
+  {
+    "author": "Unknown",
+    "country": "Sumer and Akkadian Empire",
+    "imageLink": "images/the-epic-of-gilgamesh.jpg",
+    "language": "Akkadian",
+    "link": "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh\n",
+    "pages": 160,
+    "title": "The Epic Of Gilgamesh",
+    "year": -1700
+  },
+  {
+    "author": "Unknown",
+    "country": "Achaemenid Empire",
+    "imageLink": "images/the-book-of-job.jpg",
+    "language": "Hebrew",
+    "link": "https://en.wikipedia.org/wiki/Book_of_Job\n",
+    "pages": 176,
+    "title": "The Book Of Job",
+    "year": -600
+  }, {
+    "author": "Chinua Achebe",
+    "country": "Nigeria",
+    "imageLink": "images/things-fall-apart.jpg",
+    "language": "English",
+    "link": "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
+    "pages": 209,
+    "title": "Things Fall Apart",
+    "year": 1958
+  },
+  {
+    "author": "Hans Christian Andersen",
+    "country": "Denmark",
+    "imageLink": "images/fairy-tales.jpg",
+    "language": "Danish",
+    "link": "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n",
+    "pages": 784,
+    "title": "Fairy tales",
+    "year": 1836
+  },
+  {
+    "author": "Dante Alighieri",
+    "country": "Italy",
+    "imageLink": "images/the-divine-comedy.jpg",
+    "language": "Italian",
+    "link": "https://en.wikipedia.org/wiki/Divine_Comedy\n",
+    "pages": 928,
+    "title": "The Divine Comedy",
+    "year": 1315
+  },
+  {
+    "author": "Unknown",
+    "country": "Sumer and Akkadian Empire",
+    "imageLink": "images/the-epic-of-gilgamesh.jpg",
+    "language": "Akkadian",
+    "link": "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh\n",
+    "pages": 160,
+    "title": "The Epic Of Gilgamesh",
+    "year": -1700
+  },
+  {
+    "author": "Unknown",
+    "country": "Achaemenid Empire",
+    "imageLink": "images/the-book-of-job.jpg",
+    "language": "Hebrew",
+    "link": "https://en.wikipedia.org/wiki/Book_of_Job\n",
+    "pages": 176,
+    "title": "The Book Of Job",
+    "year": -600
+  }, {
+    "author": "Chinua Achebe",
+    "country": "Nigeria",
+    "imageLink": "images/things-fall-apart.jpg",
+    "language": "English",
+    "link": "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
+    "pages": 209,
+    "title": "Things Fall Apart",
+    "year": 1958
+  },
+  {
+    "author": "Hans Christian Andersen",
+    "country": "Denmark",
+    "imageLink": "images/fairy-tales.jpg",
+    "language": "Danish",
+    "link": "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n",
+    "pages": 784,
+    "title": "Fairy tales",
+    "year": 1836
+  },
+  {
+    "author": "Dante Alighieri",
+    "country": "Italy",
+    "imageLink": "images/the-divine-comedy.jpg",
+    "language": "Italian",
+    "link": "https://en.wikipedia.org/wiki/Divine_Comedy\n",
+    "pages": 928,
+    "title": "The Divine Comedy",
+    "year": 1315
+  },
+  {
+    "author": "Unknown",
+    "country": "Sumer and Akkadian Empire",
+    "imageLink": "images/the-epic-of-gilgamesh.jpg",
+    "language": "Akkadian",
+    "link": "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh\n",
+    "pages": 160,
+    "title": "The Epic Of Gilgamesh",
+    "year": -1700
+  },
+  {
+    "author": "Unknown",
+    "country": "Achaemenid Empire",
+    "imageLink": "images/the-book-of-job.jpg",
+    "language": "Hebrew",
+    "link": "https://en.wikipedia.org/wiki/Book_of_Job\n",
+    "pages": 176,
+    "title": "The Book Of Job",
+    "year": -600
+  }, {
+    "author": "Chinua Achebe",
+    "country": "Nigeria",
+    "imageLink": "images/things-fall-apart.jpg",
+    "language": "English",
+    "link": "https://en.wikipedia.org/wiki/Things_Fall_Apart\n",
+    "pages": 209,
+    "title": "Things Fall Apart",
+    "year": 1958
+  },
+  {
+    "author": "Hans Christian Andersen",
+    "country": "Denmark",
+    "imageLink": "images/fairy-tales.jpg",
+    "language": "Danish",
+    "link": "https://en.wikipedia.org/wiki/Fairy_Tales_Told_for_Children._First_Collection.\n",
+    "pages": 784,
+    "title": "Fairy tales",
+    "year": 1836
+  },
+  {
+    "author": "Dante Alighieri",
+    "country": "Italy",
+    "imageLink": "images/the-divine-comedy.jpg",
+    "language": "Italian",
+    "link": "https://en.wikipedia.org/wiki/Divine_Comedy\n",
+    "pages": 928,
+    "title": "The Divine Comedy",
+    "year": 1315
+  },
+  {
+    "author": "Unknown",
+    "country": "Sumer and Akkadian Empire",
+    "imageLink": "images/the-epic-of-gilgamesh.jpg",
+    "language": "Akkadian",
+    "link": "https://en.wikipedia.org/wiki/Epic_of_Gilgamesh\n",
+    "pages": 160,
+    "title": "The Epic Of Gilgamesh",
+    "year": -1700
+  },
+  {
+    "author": "Unknown",
+    "country": "Achaemenid Empire",
+    "imageLink": "images/the-book-of-job.jpg",
+    "language": "Hebrew",
+    "link": "https://en.wikipedia.org/wiki/Book_of_Job\n",
+    "pages": 176,
+    "title": "The Book Of Job",
+    "year": -600
+  },
+  {
+    "author": "Unknown",
+    "country": "Achaemenid Empire",
+    "imageLink": "images/the-book-of-job.jpg",
+    "language": "Hebrew",
+    "link": "https://en.wikipedia.org/wiki/Book_of_Job\n",
+    "pages": 176,
+    "title": "The Book Of Job",
+    "year": -600
+  },
+  {
+    "author": "Dante Alighieri",
+    "country": "Italy",
+    "imageLink": "images/the-divine-comedy.jpg",
+    "language": "Italian",
+    "link": "https://en.wikipedia.org/wiki/Divine_Comedy\n",
+    "pages": 928,
+    "title": "The Divine Comedy",
+    "year": 1315
+  },
+]
 
 //PARA EL ANIDAMIENTO USAREMOS VARIABLES QUE DECLAREN CON SU NOMBRE LA POSICION DE ANIDAMIENTO CON RESPECTO AL BODY
 
@@ -56,14 +374,14 @@ const books = [
 const padreBd = document.querySelector("body")
 
 //creamos a header, main y footer
-const mmaaiinn = document.createElement("main")
-const hheeaaddeerr = document.createElement("header")
-const ffooootteerr = document.createElement("footer")
+const cabeza = document.createElement("header")
+const cuerpo = document.createElement("main")
+const pie = document.createElement("footer")
 
 //Los anidamos en body (padreBd) y declaramos a main como hijoBd
-padreBd.appendChild(mmaaiinn)
-padreBd.appendChild(hheeaaddeerr)
-padreBd.appendChild(ffooootteerr)
+padreBd.appendChild(cabeza)
+padreBd.appendChild(cuerpo)
+padreBd.appendChild(pie)
 const hijoBd = document.querySelector('main')
 
 //Creamos el section, lo anidamos en main (hijoBd) y declaramos al seccion como nietoBd
@@ -73,10 +391,27 @@ const nietoBd = document.querySelector("section")
 
 //Creamos un bucle for que itera tantas veces como objetos tenga el array Books
 for (let i = 0; i < books.length; i++) {
+
+  //Este if crea un div cada tres iteracciones para asi poder usar contenedores flexbox que almacenen los libros de tres en tres (se podria cambiar por otra cantidad)
+  if (i % 3 == 0) {
+    const div03 = document.createElement("div")
+    nietoBd.appendChild(div03)
+  }
+  //Este if else asigna a la variable indiceDiv el indice del div. Dicho indice sirve para que los articles se aniden en su div correspondiente. No podemos usar i, pues esta va sumando uno cada iteraccion, mientras que el indice del div solo suma 1 cada tres iteracciones.
+
+  //El metodo Math.trunc elimina los decimales del numero
+  let indiceDiv = 0;
+  if (i % 3 == 0) {
+    indiceDiv = i / 3
+  }
+  else {
+    indiceDiv = Math.trunc(i / 3)
+  }
+  const bisnietoBd = document.getElementsByTagName("div")[indiceDiv]
   //Creamos los articulos, los anidamos en seccion (nietoBd) y declaramos cada articulo  como bisnietoBd 
   const articulo = document.createElement("article")
-  nietoBd.appendChild(articulo)
-  const bisnietoBd = document.getElementsByTagName("article")[i]
+  bisnietoBd.appendChild(articulo)
+  const tataranietoBd = document.getElementsByTagName("article")[i]
   //Creamos los elementos de texto e imagen:
   const titulo = document.createElement("h3")
   const autor = document.createElement("p")
@@ -85,11 +420,12 @@ for (let i = 0; i < books.length; i++) {
   const idioma = document.createElement("p")
   const año = document.createElement("p")
   //Los anidamos en article (bisnietoBd):
-  bisnietoBd.appendChild(titulo)
-  bisnietoBd.appendChild(pais)
-  bisnietoBd.appendChild(portada)
-  bisnietoBd.appendChild(idioma)
-  bisnietoBd.appendChild(año)
+  tataranietoBd.appendChild(titulo)
+  tataranietoBd.appendChild(autor)
+  tataranietoBd.appendChild(pais)
+  tataranietoBd.appendChild(portada)
+  tataranietoBd.appendChild(idioma)
+  tataranietoBd.appendChild(año)
 
   //Le damos valor a cada elemento de texto e imagen
 
@@ -100,3 +436,36 @@ for (let i = 0; i < books.length; i++) {
   idioma.innerHTML = "Language: " + books[i].language
   año.innerHTML = "Year: " + books[i].year
 }
+
+
+//La funcion Cuadrar articulo crea articulos en caso de que el indice de books(numero de targetas) no sea multiplo de tres, de esta forma flex box cuadra los elementos todos con el mismo tamaño.
+let y = Math.trunc((books.length) / 3)
+
+const bisnietoBd2 = document.getElementsByTagName("div")[y]
+
+function cuadrarArticulo() {
+  if ((books.length + 1) % 3 == 0) {
+    const articulo = document.createElement("article")
+    bisnietoBd2.appendChild(articulo)
+  }
+
+  if ((books.length + 2) % 3 == 0) {
+    const articulo = document.createElement("article")
+    bisnietoBd2.appendChild(articulo)
+  }
+
+  if ((books.length + 2) % 3 == 0) {
+    const articulo = document.createElement("article")
+    bisnietoBd2.appendChild(articulo)
+  }
+
+}
+cuadrarArticulo()
+
+
+
+
+
+
+
+
